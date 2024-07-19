a7c840882fbf79a023efec5566d988bdce96e895
AntonioP2004 <116681343+AntonioP2004@users.noreply.github.com>
2024-07-19 13:04:00 -0600

    upload main.js
    main.js creates app and launches window for program

---
+++ main.js
@@ -0,0 +1,55 @@
+// main.js
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    frame: false, // Frameless window
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+  mainWindow.setMenuBarVisibility(false);
+}
+
+// IPC handlers
+ipcMain.on('minimize-window', () => {
+  const window = BrowserWindow.getFocusedWindow();
+  if (window) window.minimize();
+});
+
+ipcMain.on('maximize-window', () => {
+  const window = BrowserWindow.getFocusedWindow();
+  if (window) {
+    if (window.isMaximized()) {
+      window.unmaximize();
+    } else {
+      window.maximize();
+    }
+  }
+});
+
+ipcMain.on('close-window', () => {
+  const window = BrowserWindow.getFocusedWindow();
+  if (window) window.close();
+});
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
